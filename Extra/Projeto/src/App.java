7540c211e0358b26c6718c7023c8f0f765179a54
Rômulo França Dev <157732149+romullo-dev@users.noreply.github.com>
2024-07-01 20:23:49 -0300

    commit

--- Extra/Projeto/src/App.java
+++ Extra/Projeto/src/App.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
 
-public class App {
+public class App extends JFrame {
+    //CRINDO COMPONENTES
+    static JLabel text = new JLabel("SUSHI HOJE??"); 
+    static JButton ButtonSim =  new JButton("SIM 😊");
+    static JButton ButtonNao = new JButton("NÃO 😒");
+
     public static void main(String[] args) throws Exception {
-        JFrame Form = new JFrame ();
-        Form.
+        App window = new App();
+        Window
+        
     }
 }
