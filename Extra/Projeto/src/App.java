65a339bc833df672d0ddf76127fc1622a3427271
Rômulo França Dev <romulo.franca03@gmail.com>
2024-07-13 20:39:14 -0300

    C

--- Extra/Projeto/src/App.java
+++ Extra/Projeto/src/App.java
@@ -2,13 +2,25 @@ import javax.swing.*;
 
 public class App extends JFrame {
     //CRINDO COMPONENTES
-    static JLabel text = new JLabel("SUSHI HOJE??"); 
+    static JLabel text = new JLabel("SUSHI HOJE??");
     static JButton ButtonSim =  new JButton("SIM 😊");
     static JButton ButtonNao = new JButton("NÃO 😒");
 
     public static void main(String[] args) throws Exception {
         App window = new App();
-        Window
-        
+        window.setSize(500,500);
+        window.setVisible(true);
+
+
+        JPanel Painel = new JPanel();
+        window.add(Painel);
+
+        Painel.add(text);
+
+        Painel.add(ButtonSim);
+        Painel.add(ButtonNao);
+
+
+
     }
 }
